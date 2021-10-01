054f0dba255b2b25fd077e40d7ed8e3f7a9b5e4c
Krasimir Hristov <krasimir.hristov@nexo.io>
2021-10-01 17:36:14 +0300

    Refactored Register page as page object

--- cypress/integration/3-nexo-example/createAccount.spec.js
+++ cypress/integration/3-nexo-example/createAccount.spec.js
@@ -24,26 +24,58 @@ cy.visit('https://automationteststore.com//',)
 
         cy.get('#accountFrm > fieldset > .btn').click()
         // cy.get('#AccountFrm_firstname').type('Jeki')
-        register.populateUserName('Jeki')
+        //cy.get('#AccountFrm_lastname').type('Jeki')
+        register.populateUserName('Jeki','Jeki')
 
-        cy.get('#AccountFrm_lastname').type('Jeki')
-        cy.get('#AccountFrm_email').type(testname)
-        cy.get('#AccountFrm_telephone').type('0885564565')
-        cy.get('#AccountFrm_fax').type('223475698')
-        cy.get('#AccountFrm_company').should('be.visible').type('Kolega OOD')
-        cy.get('#AccountFrm_address_1').should('be.visible').type('Address 1')
-        cy.get('#AccountFrm_address_2').should('be.visible').type('Address 2')
-        cy.get('#AccountFrm_city').should('be.visible').type('Moscow')
-        cy.get('#AccountFrm_postcode').should('be.visible').type('121')
-        cy.get('#AccountFrm_country_id').should('be.visible').select('Russia')
-        cy.get('#AccountFrm_zone_id').should('be.visible').select('Moscow')
-        cy.get('#AccountFrm_loginname').should('be.visible').type(`Jekata${id}`)
-        cy.get('#AccountFrm_password').should('be.visible').type('pass123')
-        cy.get('#AccountFrm_confirm').should('be.visible').type('pass123')
-        cy.get('#AccountFrm_newsletter0').check()
-        cy.get('#AccountFrm_agree').check()
-        cy.get('.col-md-2 > .btn').click()
-        cy.get('.mb40 > :nth-child(3)').should('have.text', 'Congratulations! Your new account has been successfully created!')
+        //cy.get('#AccountFrm_email').type(testname)
+        register.populateEmail(testname)
+
+        //cy.get('#AccountFrm_telephone').type('0885564565')
+        register.fillInTelephone('0885564565')
+
+        //cy.get('#AccountFrm_fax').type('223475698')
+        register.fillInFax('223475698')
+
+        //cy.get('#AccountFrm_company').should('be.visible').type('Kolega OOD')
+        register.fillInCompanyName('Kolega OOD')
+
+        //cy.get('#AccountFrm_address_1').should('be.visible').type('Address 1')
+        //cy.get('#AccountFrm_address_2').should('be.visible').type('Address 2')
+        register.fillInCompanyAddress('Address 1', 'Address 2')
+
+        // cy.get('#AccountFrm_city').should('be.visible').type('Moscow')
+        register.fillInCity('Moscow')
+        // cy.get('#AccountFrm_postcode').should('be.visible').type('121')
+        register.fillInPostCode('121')
+
+        // cy.get('#AccountFrm_country_id').should('be.visible').select('Russia')
+        register.fillInCountry('Russia')
+
+        //cy.get('#AccountFrm_zone_id').should('be.visible').select('Moscow')
+        register.selectCity('Moscow')
+
+        //cy.get('#AccountFrm_loginname').should('be.visible').type(`Jekata${id}`)
+        register.fillInUserId(`Jekata${id}`)
+
+
+        //cy.get('#AccountFrm_password').should('be.visible').type('pass123')
+        register.fillInPass('pass123')
+
+
+        //cy.get('#AccountFrm_confirm').should('be.visible').type('pass123')
+        register.confirmPass('pass123')
+
+        //cy.get('#AccountFrm_newsletter0').check()
+        register.checkNewsCheckbox()
+
+        //cy.get('#AccountFrm_agree').check()
+        register.checkAgreeCheckbox()
+
+        //cy.get('.col-md-2 > .btn').click()
+        register.submitRegistration()
+
+        //cy.get('.mb40 > :nth-child(3)').should('have.text', 'Congratulations! Your new account has been successfully created!')
+        register.assertRegistrationText('Congratulations! Your new account has been successfully created!')
 
     })
 })
--- cypress/support/PageObjects/RegisterPage.js
+++ cypress/support/PageObjects/RegisterPage.js
@@ -1,7 +1,73 @@
 class RegisterPage {
 
-populateUserName(user){
-    cy.get('#AccountFrm_firstname').type(user)
+populateUserName(userF, userL){
+    cy.get('#AccountFrm_firstname').type(userF)
+    cy.get('#AccountFrm_lastname').type(userL)
+}
+
+populateEmail(email){
+    cy.get('#AccountFrm_email').type(email)
+}
+
+fillInTelephone(telNum){
+    cy.get('#AccountFrm_telephone').type(telNum)
+}
+
+fillInFax(faxNum){
+    cy.get('#AccountFrm_fax').type(faxNum)
+}
+
+fillInCompanyName(companyName){
+    cy.get('#AccountFrm_company').should('be.visible').type(companyName)
+}
+
+fillInCompanyAddress(companyAddress1, companyAddress2){
+    cy.get('#AccountFrm_address_1').should('be.visible').type(companyAddress1)
+    cy.get('#AccountFrm_address_2').should('be.visible').type(companyAddress2)
+}
+
+fillInCity(cityName){
+    cy.get('#AccountFrm_city').should('be.visible').type(cityName)
+}
+
+fillInPostCode(pCode){
+    cy.get('#AccountFrm_postcode').should('be.visible').type(pCode) 
+}
+
+fillInCountry(countryName){
+    cy.get('#AccountFrm_country_id').should('be.visible').select(countryName)
+}
+
+selectCity(city){
+    cy.get('#AccountFrm_zone_id').should('be.visible').select(city)
+}
+
+fillInUserId(id){
+    cy.get('#AccountFrm_loginname').should('be.visible').type(id)
+}
+
+fillInPass(pass){
+    cy.get('#AccountFrm_password').should('be.visible').type(pass)
+}
+
+confirmPass(pass){
+    cy.get('#AccountFrm_confirm').should('be.visible').type(pass)
+}
+
+checkNewsCheckbox(){
+    cy.get('#AccountFrm_newsletter0').check()
+}
+
+checkAgreeCheckbox(){
+    cy.get('#AccountFrm_agree').check()
+}
+
+submitRegistration(){
+    cy.get('.col-md-2 > .btn').click()
+}
+
+assertRegistrationText(regText){
+    cy.get('.mb40 > :nth-child(3)').should('have.text', regText)
 }
 
 
